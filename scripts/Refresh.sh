24a259b8eed72d64d6515cca1567b264bb6447ca
Ameer Arsala <ameer.arsala03@gmail.com>
2025-07-27 03:21:14 -0700

    adapted to new end-4 ver

--- scripts/Refresh.sh
+++ scripts/Refresh.sh
@@ -7,23 +7,23 @@ UserScripts=$HOME/.config/hypr/UserScripts
 
 # Define file_exists function
 file_exists() {
-    if [ -e "$1" ]; then
-        return 0  # File exists
-    else
-        return 1  # File does not exist
-    fi
+  if [ -e "$1" ]; then
+    return 0 # File exists
+  else
+    return 1 # File does not exist
+  fi
 }
 
 # Kill already running processes
 _ps=(waybar rofi swaync ags)
 for _prs in "${_ps[@]}"; do
-    if pidof "${_prs}" >/dev/null; then
-        pkill "${_prs}"
-    fi
+  if pidof "${_prs}" >/dev/null; then
+    pkill "${_prs}"
+  fi
 done
 
 # quit ags
-ags -q
+#ags -q
 
 sleep 0.3
 #Restart waybar
@@ -31,16 +31,16 @@ waybar &
 
 # relaunch swaync
 sleep 0.5
-swaync > /dev/null 2>&1 &
+swaync >/dev/null 2>&1 &
 
 # relaunch ags
-ags &
+#ags &
 
 # Relaunching rainbow borders if the script exists
 sleep 1
 if file_exists "${UserScripts}/RainbowBorders.sh"; then
-    ${UserScripts}/RainbowBorders.sh &
+  ${UserScripts}/RainbowBorders.sh &
 fi
 
-
 exit 0
+
